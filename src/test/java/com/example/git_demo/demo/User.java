f7c43e6e72d7220b2a5e284e0453363c714c7a3b
pranavdeshpande197 <deshpandepranav197@gmail.com>
2025-02-15 16:11:47 +0530

    adding user entity

---
+++ src/test/java/com/example/git_demo/demo/User.java
@@ -0,0 +1,31 @@
+package com.example.git_demo.demo;
+
+public class User {
+    private int userId;
+    private String userName;
+    private String userEmail;
+
+    public int getUserId()
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
